f535cc2f5c5f0f9e1922b84fa49e3e8516ac84cd
HUGO <mlgm1979@gmail.com>
2023-10-05 18:53:40 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import adivinanzas from "./adivinanzas.json";
 import './App.css';
 
 function App() {
-  var [t, setT]=useState("")
-  var [yesOrNo, yesOrNoSet] = useState("")
-  var advina = adivinanzas[Math.floor(Math.random() * adivinanzas.length)]
+  const [t, setT] = useState("");
+  const [yesOrNo, yesOrNoSet] = useState("");
+  const [advina, setAdvina] = useState(null);
+
+  // Función para seleccionar una adivinanza aleatoria al inicio
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * adivinanzas.length);
+    setAdvina(adivinanzas[randomIndex]);
+  }, []);
+
+  const handleCorregirClick = () => {
+    if (t === advina.res) {
+      yesOrNoSet("Correcto, reiniciando la página en 2 segundos...");
+      // Espera 5 segundos antes de recargar la página
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } else {
+      yesOrNoSet("Incorrecto, inténtalo otra vez");
+    }
+  };
+
   return (
     <div className="App">
-      <p>Hola introduce la respuesta de la adivinaza y haz doble click en la caja de texto para corregir</p>
-      <h1>{advina.req}</h1>
-      <input placeholder="respuesta" onChange={e => { setT(e.target.value); } } />
-      <button onClick={() => {if (t === advina.res){
-          yesOrNoSet("Correcto, reinicia la pagina para obtener otra adivinaza")
-        }else{
-          yesOrNoSet("Incorrecto, intentalo otra vez")
-        }} }>Coregir</button><p>{yesOrNo}</p>
+      <p>Hola introduce la respuesta de la adivinanza y haz doble click en la caja de texto para corregir</p>
+      {advina && <h1>{advina.req}</h1>}
+      <input placeholder="respuesta" onChange={e => setT(e.target.value)} />
+      <button onClick={handleCorregirClick}>Corregir</button>
+      <p>{yesOrNo}</p>
     </div>
   );
 }
